6442cc7eac86ddb73a9250b7b0c7f6ae26e1912d
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2021-10-06 10:44:17 -0400

    added id to employees table

--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,7 @@ DROP TABLE IF EXISTS department;
 
 
 CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     role_id INTEGER NOT NULL,
@@ -12,6 +13,7 @@ CREATE TABLE employee (
 
 CREATE TABLE roles (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
     department_id INTEGER NOT NULL,
 );
