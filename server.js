9c8897c0463c80ab9b237b57266dda36ff2073d5
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2021-10-06 09:27:25 -0400

    added testing

--- server.js
+++ server.js
@@ -9,7 +9,28 @@ const apiRoutes = require('./routes/apiRoutes');
 
 const app = express();
 
+const welcome = () => {
+    return inquirer.prompt([{
+        type: 'input',
+        name: 'start',
+        message: `
+            ====================
+            Employee Manager Pro
+            ====================
+           Press Enter to continue`
+    },])
+    .then(mainMenu)
+};
 
+const mainMenu = () => {
+    return inquirer.prompt([{
+        type: 'list',
+        name: 'menuChoices',
+        message: 'What would you like to do?',
+        choices: ['View All Employees', 'View All Employees By Department', 'View All Employees Manager', 'Add Employee', 'Remove Employee', 'Update Employee Role', 'Update Employee Manager' ]
+    }]
+    .then())
+}
 
 
 
