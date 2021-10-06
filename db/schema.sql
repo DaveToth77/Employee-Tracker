2859cc91b15790b110e6136fcfd6a820508235ca
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2021-10-06 09:54:16 -0400

    created tables

---
+++ db/schema.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS department;
+
+
+CREATE TABLE employee (
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER NOT NULL
+);
+
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INTEGER NOT NULL,
+);
+
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+);
