234a57c52f2822ef98df28eb1cb7cad5c8de7fb1
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2021-10-06 12:24:50 -0400

    Added mainMenu switch/case

--- server.js
+++ server.js
@@ -1,41 +1,83 @@
-
 const cTable = require('console.table');
 const mysql = require('mysql2');
-const express = require('express');
 const inquirer = require('inquirer');
-const PORT = process.env.PORT || 3000;
 
 const apiRoutes = require('./routes/apiRoutes');
 
-const app = express();
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'public00',
+    database: 'employees'
+});
 
-const welcome = () => {
-    return inquirer.prompt([{
-        type: 'input',
-        name: 'start',
-        message: `
-            ====================
-            Employee Manager Pro
-            ====================
-           Press Enter to continue`
-    },])
-    .then(mainMenu)
-};
+Console.log(`
+_____                _                         _____              _             
+|  ___|              | |                       |_   _|            | |            
+| |__ _ __ ___  _ __ | | ___  _   _  ___  ___    | |_ __ __ _  ___| | _____ _ __ 
+|  __| '_ `
+    _\ | '_ \| |/ _ \| | | |/ _ \/ _ \   | | '
+    __ / _ ` |/ __| |/ / _ | '__|
+| |__| | | | | | |_) | | (_) | |_| |  __|  __/   | | | | (_| | (__|   |  __| |   
+\____|_| |_| |_| .__/|_|\___/ \__, |\___|\___|   \_|_|  \__,_|\___|_|\_\___|_|   
+               | |             __/ |                                             
+               |_|            |___/                                               ` + '\n'
+);
+
+connection.connect(function (err) {
+    if (err) throw err;
+    mainMenu();
+});
 
 const mainMenu = () => {
     return inquirer.prompt([{
-        type: 'list',
-        name: 'menuChoices',
-        message: 'What would you like to do?',
-        choices: ['View All Employees', 'View All Employees By Department', 'View All Employees Manager', 'Add Employee', 'Remove Employee', 'Update Employee Role', 'Update Employee Manager' ]
-    }]
-    .then())
+            type: 'list',
+            name: 'menuChoices',
+            message: 'What would you like to do?',
+            choices: [
+                'View All Departments',
+                'View All Roles',
+                'View All Employees',
+                'Add Department',
+                'Add a Role',
+                'Add an Employee',
+                'Update Employee Role',                
+                'Exit'
+            ]
+        }]
+        .then(answer => {
+            switch (answer.mainMenu) {
+                case "View All Employees":
+                    viewAllEmployees();
+                    break;
+
+                case "View All Departments":
+                    viewAllDept();
+                    break;
+
+                case "View All Roles":
+                    viewAllRoles();
+                    break;
+
+                case "Add an Employee":
+                    addEmployee();
+                    break;
+
+                case "Add Department":
+                    addDept();
+                    break;
+
+                case "Add Role":
+                    addRole();
+                    break;
+
+                case "Update Employee Role":
+                    updateEmployeeRole();
+                    break;
+
+                case "Exit":
+                    connection.end();
+                    break;
+            }
+        }))
 }
-
-
-
-
-
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}!`);
-});
